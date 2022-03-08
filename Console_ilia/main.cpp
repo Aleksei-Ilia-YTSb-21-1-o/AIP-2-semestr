ca6043fdbee44dd0b73e5aac4c4afc916212db2c
Aleksei-Ilia-YTSb-21-1-o <93817340+Aleksei-Ilia-YTSb-21-1-o@users.noreply.github.com>
2022-03-08 15:10:10 +0300

    Add files via upload

--- Console_ilia/main.cpp
+++ Console_ilia/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cmath>
-const float PI = 3.141592653589793238;
+//const float PI = 3.141592653589793238;
 
-int main(int answer0) {
+//int main(int answer0) {
 	/*std::cout << "Alexei " << 17 << std::endl
 	<< "Kosta " <<21<< std::endl
 	<<"Igor "  <<19<< std::endl;
@@ -30,7 +30,7 @@ int main(int answer0) {
 	return 0;
 	*/
 	
-	std::cout << "\tOption 7\nCompleted tasks: 1 (1);2g(2);5(3)"<< std::endl;
+	/*std::cout << "\tOption 7\nCompleted tasks: 1 (1);2g(2);5(3)" << std::endl;
     do
     {
 		
@@ -70,17 +70,91 @@ int main(int answer0) {
 			answer_sum = one + two + three;
 			std::cout << "conversion\n" << one << "\t" << two << "\t" << three << "\t" << "sum of converted numbers: " << answer_sum << std::endl;
 
-		}
+		}*/
 		/*std::cout << "Will they return to the task selection?\n Yes(1) no(0) " << std::endl;
 		int answer;
 		std::cin >> answer;
 	  answer = answer0;*/
 		//была идея но я её не реализовал. был большооооой костыль который и не работал. хотя, сейчас прога работая на костыле. но работает!!!))) 
 
-	} while (answer0 == 1);
+	/* } while (answer0 == 1);
 	
 	if (answer0 == 0)
 		exit(1);
 	
 	return(0);
-} 
+} */
+
+// G - градус; R - радиан;  m - минута;     h - час; _ - перевод; 
+const float  Gm_Gh = 60;
+const float Gh_Gm = 0.0166666667;
+const float Gm_Rm = 0.0174532925;
+const float Rm_Gm = 57.2957795;
+const float Gh_Rm = 0.000290888209;
+const float Rm_Gh = 3437.74677;
+
+int main() { // задание 5
+	int Input_Conversion, Output_Conversion;
+	float Meaning, Onsver{};
+	std::cout <<"Degrees per hour(1); degrees per minute (2); Radians per minute (3):First enter the original Vedic value (its number) after the value of the card and after its transfer number. if an error is displayed, it means that you have entered the wrong number of values." << std::endl;
+	std::cin >> Input_Conversion >> Meaning >> Output_Conversion;
+
+	if (Input_Conversion == Output_Conversion)
+	Onsver = Meaning;
+	
+		
+	if ((Input_Conversion == 1) && (Output_Conversion == 2))
+		Onsver = Meaning * Gh_Gm;
+	
+
+	if ((Input_Conversion == 1) && (Output_Conversion == 3))
+		Onsver = Meaning * Gh_Rm;
+			
+	if ((Input_Conversion == 2) && (Output_Conversion == 1))
+		Onsver = Meaning * Gm_Gh;
+
+	if ((Input_Conversion == 2) && (Output_Conversion == 3))
+		Onsver = Meaning * Gm_Rm;
+		
+	if ((Input_Conversion == 3) && (Output_Conversion == 1))
+		Onsver = Meaning * Rm_Gh;
+
+	if ((Input_Conversion == 3) && (Output_Conversion == 2))
+		Onsver = Meaning * Rm_Gm;
+
+	if ((Input_Conversion > 3) || (Output_Conversion > 3) || (Input_Conversion < 1) || (Output_Conversion < 1))
+		std::cout << "Error" <<  std::endl;
+
+	std::cout << Onsver << std::endl;
+
+}
+
+int main()
+{
+
+	float a = 2, b = 3, j;			//задание 4
+	j = (a + 4 * b) * (a - 3 * b) + (a * a);
+	std::cout << j << std::endl;
+}
+
+int main() // задание 6
+{	
+	float  alfa, betta, gamma;
+	std::cin >> alfa, betta, gamma;
+	if ((alfa > betta) && (alfa > gamma))
+		std::cout << alfa << std::endl;
+	else if ((betta > gamma) && (betta > gamma))
+		std::cout << betta << std::endl;
+	else if ((gamma > alfa) && (gamma > betta))
+		std::cout << betta << std::endl;
+	else if ((betta > gamma) && (betta > gamma))
+		std::cout << gamma << std::endl;
+	else if ((alfa > gamma) && (betta = alfa))
+		std::cout << betta << " " << alfa << std::endl;
+	else if ((betta > alfa) && (betta = gamma))
+		std::cout << betta << " " << gamma << std::endl;
+	else if ((alfa > betta) && (betta = alfa))
+		std::cout << betta << " " << alfa << std::endl;
+	else std::cout << "they are equal" << std::endl;
+
+}
